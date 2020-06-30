9e12a6d8e12f216ade7663c8b9459f322905a407
Ashish Khanal <ashishkhanal@Ashishs-MacBook-Pro.local>
2020-06-29 23:03:09 -0400

    event handling lesson

--- main.js
+++ main.js
@@ -4,6 +4,28 @@ var app = new Vue({
         product: 'Socks',
         image: './assets/vmSocks-green-onWhite.jpg',
         altText: 'A pair of socks',
-        inventory: 8
+        inventory: 8,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants:[
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: "./assets/vmSocks-green-onWhite.jpg"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+            }
+        ],
+        cart: 0
+    },
+    methods:{
+        addToCart : function(){
+            this.cart++; //this refers to the data of the instance we’re currently in
+        },
+        changeSocksColor : function(variantImage){
+            this.image = variantImage;
+        }
     }
 })
