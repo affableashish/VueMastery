359fa0899e7cf8b48fa4cd286b209a70dd2cba85
Ashish Khanal <ashishkhanal@Ashishs-MacBook-Pro.local>
2020-07-12 11:18:25 -0400

    class and style binding

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var app = new Vue({
         image: './assets/vmSocks-green-onWhite.jpg',
         altText: 'A pair of socks',
         inventory: 8,
+        inStock: true,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants:[
             {
